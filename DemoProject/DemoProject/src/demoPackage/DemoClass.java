a301c57070356a47d0d7e4fff38318156cdfd6be
asha10793 <asha.chaurasia10793@gmail.com>
2023-01-28 00:51:22 +0530

    basic task

---
+++ DemoProject/DemoProject/src/demoPackage/DemoClass.java
@@ -0,0 +1,12 @@
+package demoPackage;
+
+public class DemoClass {
+	public static void main(String args[])
+	{
+		System.out.println("TestBase");
+		System.out.println("pom.xml");
+		System.out.println("Utilities");
+		System.out.println("Resources");
+		System.out.println("Suit");
+	}
+}
---
+++ DemoProject/DemoProject/src/demoPackage/LoginPage.java
@@ -0,0 +1,11 @@
+package demoPackage;
+
+public class LoginPage {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Task completed");
+
+	}
+
+}
